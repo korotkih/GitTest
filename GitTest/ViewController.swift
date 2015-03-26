ff18d36f0a481816a9ce7298f07d15acd7336300
Michael Korotkih <michael.korotkih@gmail.com>
2015-03-26 10:52:24 +0300

    final

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         println("2")
-        println("Second line")
+        println("Second line test")
     }
 
     override func didReceiveMemoryWarning() {
